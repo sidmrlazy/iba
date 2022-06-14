90a36e1a0c1f1f59a0bcaa6f54809da1803ccdeb
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2022-06-14 10:11:30 +0530

    Updated

--- components/server/config.php
+++ components/server/config.php
@@ -1,15 +1,15 @@
 <?php
 // Production
-// $servername = "localhost";
-// $username = "u976956619_iba";
-// $database = "u976956619_iba";
-// $password = "Sid12asthana";
+$servername = "localhost";
+$username = "u976956619_iba";
+$database = "u976956619_iba";
+$password = "Sid12asthana";
 
 // Development
-$servername = "localhost";
-$username = "root";
-$database = "iba";
-$password = "";
+// $servername = "localhost";
+// $username = "root";
+// $database = "iba";
+// $password = "";
 
 // Validate Connection
 $connection = new mysqli($servername, $username, $password, $database);
