a5a60475eefe8463cfe9cff6b040d609e34f1886
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2022-07-22 14:02:56 +0530

    modified:   components/home-page/contact.php modified:   components/server/config.php

--- components/home-page/contact.php
+++ components/home-page/contact.php
@@ -3,96 +3,173 @@
     <p class="mb-5">Mention your details below for us to contact you.</p>
     <?php
     include('components/server/config.php');
-
     if (isset($_POST['submit'])) {
-        $contact_name = $_POST['contact_name'];
-        $contact_number = $_POST['contact_number'];
-        $contact_email = $_POST['contact_email'];
-        $contact_state = $_POST['contact_state'];
-        $contact_city = $_POST['contact_city'];
-        $contact_pincode = $_POST['contact_pincode'];
-        $contact_address = $_POST['contact_address'];
+        $user_name = $_POST['user_name'];
+        $user_contact = $_POST['user_contact'];
+        $user_contact_whatsapp = $_POST['user_contact_whatsapp'];
+        $user_property_type = $_POST['user_property_type'];
+        $user_floor_offered = $_POST['user_floor_offered'];
+        $user_property_area = $_POST['user_property_area'];
+        $user_property_frontage = $_POST['user_property_frontage'];
+        $user_property_landmark = $_POST['user_property_landmark'];
+        $user_state = $_POST['user_state'];
+        $user_city = $_POST['user_city'];
+        $user_pincode = $_POST['user_pincode'];
 
-        $query = "INSERT INTO `contact`(
-            `contact_name`, 
-            `contact_number`, 
-            `contact_email`, 
-            `contact_state`, 
-            `contact_city`, 
-            `contact_pincode`, 
-            `contact_address`) VALUES (
-                '$contact_name',
-                '$contact_number',
-                '$contact_email',
-                '$contact_state',
-                '$contact_city',
-                '$contact_pincode',
-                '$contact_address')";
 
+        $query = "INSERT INTO `user_form`(
+            `user_name`, 
+            `user_contact`, 
+            `user_contact_whatsapp`, 
+            `user_property_type`, 
+            `user_floor_offered`, 
+            `user_property_area`, 
+            `user_property_frontage`, 
+            `user_property_landmark`, 
+            `user_state`, 
+            `user_city`, 
+            `user_pincode`) VALUES (
+                '$user_name',
+                '$user_contact',
+                '$user_contact_whatsapp',
+                '$user_property_type',
+                '$user_floor_offered',
+                '$user_property_area',
+                '$user_property_frontage',
+                '$user_property_landmark',
+                '$user_state',
+                '$user_city',
+                '$user_pincode')";
         $result = mysqli_query($connection, $query);
-
-        $to = "ibasthana@gmail.com";
-        $subject = "ibasthana.com: Website Visitor!";
-        $message = $contact_name . " " . "has contacted you on your website. His Email Address is " . $contact_email . " and his address is " . $contact_address . ", " . $contact_city . ", " . $contact_pincode;
-
-        if ($result) {
-            mail($to, $subject, $message);
-            echo "<div class='alert alert-success text-center col-md-12' role='alert'>
-            Form Submitted! Thank you.
-          </div>";
+        if (!$result) {
+            die("<div class='alert alert-danger' role='alert'>
+            Error! There was some problem in submitting this form.
+          </div>" . mysqli_error($connection));
         } else {
-            die("ERROR 404!" . mysqli_error($connection));
+            echo "<div class='alert alert-success' role='alert'>Thank you for your interest. Someone from our team will contact you shortly</div>";
         }
     }
+
     ?>
     <form method="POST" action="" class="col-md-12 contact-form">
         <div class="col-md-12 form-floating mb-3">
-            <input type="text" name="contact_name" class="form-control" required id="floatingInput"
+            <input type="text" name="user_name" class="form-control" required id="floatingInput"
                 placeholder="Full Name">
-            <label for="floatingInput">Full Name</label>
+            <label for="floatingInput">Name of Owner</label>
         </div>
         <div class="col-md-12 form-floating mb-3">
-            <input type="number" name="contact_number" required class="form-control" id="floatingNumber"
+            <input type="number" name="user_contact" required class="form-control" id="floatingNumber"
                 placeholder="+91 XXXXX XXXXX">
-            <label for="floatingNumber">Mobile Number</label>
+            <label for="floatingNumber">Contact Number</label>
         </div>
 
-        <div class="form-floating col-md-12 mb-3">
-            <input type="email" name="contact_email" class="form-control" id="floatingEmail" placeholder="abc@xyz.com">
-            <label for="floatingEmail">Email</label>
-        </div>
 
-        <div class="form-floating col-md-12 mb-3">
-            <input type="text" name="contact_state" class="form-control" id="floatingEmail" placeholder="abc@xyz.com">
-            <label for="floatingEmail">State</label>
-        </div>
-
-        <div class="form-floating col-md-12 mb-3">
-            <input type="text" name="contact_city" class="form-control" id="floatingEmail" placeholder="abc@xyz.com">
-            <label for="floatingEmail">City</label>
-        </div>
-
-        <div class="form-floating col-md-12 mb-3">
-            <input type="number" name="contact_pincode" required class="form-control" id="floatingEmail"
-                placeholder="abc@xyz.com">
-            <label for="floatingEmail">Pincode</label>
+        <div class="form-floating mb-3">
+            <select class="form-select" id="floatingSelect" name="user_contact_whatsapp" required
+                aria-label="Floating label select example">
+                <option>Open this select menu</option>
+                <option name="user_contact_whatsapp" value="Yes">Yes</option>
+                <option name="user_contact_whatsapp" value="No">No</option>
+            </select>
+            <label for="floatingSelect">Is this number available for Whatsapp?</label>
         </div>
 
         <div class="form-floating mb-3">
-            <textarea name="contact_address" class="form-control" required placeholder="Leave a comment here"
-                id="floatingTextarea2" style="height: 100px"></textarea>
-            <label for="floatingTextarea2">Full Address</label>
+            <select class="form-select" name="user_property_type" required id="floatingSelect"
+                aria-label="Floating label select example">
+                <option selected>Click here for options</option>
+                <option value="Commercial">Commercial</option>
+                <option value="Residential">Residential</option>
+                <option value="Mixed">Mixed</option>
+            </select>
+            <label for="floatingSelect">Property Type</label>
         </div>
 
-        <button type="submit" onclick="<?php echo "<script>window.open($url)</script>"; ?>" name="submit"
-            class="contact-btn">Continue</button>
+        <div class="form-floating col-md-12 mb-3">
+            <input type="text" name="user_floor_offered" class="form-control" id="floatingArea" required
+                placeholder="Floor Offered">
+            <label for="floatingArea">Floor offered</label>
+        </div>
+
+
+        <div class="form-floating col-md-12 mb-3">
+            <input type="number" name="user_property_area" class="form-control" id="floatingArea" required
+                placeholder="in SqFt">
+            <label for="floatingArea">Total Area of the property (in SqFt)</label>
+        </div>
+
+        <div class="form-floating col-md-12 mb-3">
+            <input type="number" name="user_property_frontage" class="form-control" required id="floatingFrontage"
+                placeholder="in Feet">
+            <label for="floatingFrontage">Frontage (in Feet)</label>
+        </div>
+
+        <div class="form-floating col-md-12 mb-3">
+            <input type="text" name="user_property_landmark" class="form-control" id="floatingLandmark"
+                placeholder="Landmark">
+            <label for="floatingLandmark">Landmark</label>
+        </div>
+
+        <div class="form-floating mb-3">
+            <select class="form-select" required name="user_state" id="floatingSelect"
+                aria-label="Floating label select example">
+                <option>Click here to open list of States</option>
+                <?php
+                $curl = curl_init();
+                curl_setopt_array($curl, array(
+                    CURLOPT_URL => 'https://api.countrystatecity.in/v1/countries/IN/states',
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_HTTPHEADER => array(
+                        'X-CSCAPI-KEY: eTAxUGIyaElOSm5ldE9YdDhmQTJTaWMxbEVWUVFqR1hqblZRNmRyVw=='
+                    ),
+                ));
+                $response = curl_exec($curl);
+                curl_close($curl);
+                $response_json = json_decode($response);
+                foreach ($response_json as $key) {
+                    $user_state =  $key->name; ?>
+                <option value="<?php echo $user_state; ?>"><?php echo $user_state; ?></option>
+                <?php
+                }
+                ?>
+
+            </select>
+            <label for="floatingSelect">State</label>
+        </div>
+
+        <div class="form-floating mb-3">
+            <select class="form-select" required name="user_city" id="floatingSelect"
+                aria-label="Floating label select example">
+                <option>Click here to open list of Cities</option>
+                <?php
+                $curl = curl_init();
+                curl_setopt_array($curl, array(
+                    CURLOPT_URL => 'https://api.countrystatecity.in/v1/countries/IN/cities',
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_HTTPHEADER => array(
+                        'X-CSCAPI-KEY: eTAxUGIyaElOSm5ldE9YdDhmQTJTaWMxbEVWUVFqR1hqblZRNmRyVw=='
+                    ),
+                ));
+                $response = curl_exec($curl);
+                curl_close($curl);
+                $response_json = json_decode($response);
+                foreach ($response_json as $key) {
+                    $user_city =  $key->name; ?>
+
+                <option value="<?php echo $user_city; ?>"><?php echo $user_city; ?></option>
+                <?php
+                }
+                ?>
+            </select>
+            <label for="floatingSelect">City</label>
+        </div>
+
+        <div class="form-floating col-md-12 mb-3">
+            <input type="number" name="user_pincode" required class="form-control" id="floatingEmail"
+                placeholder="6 Digit Pincode">
+            <label for="floatingEmail">Pincode</label>
+        </div>
+
+        <button type="submit" name="submit" class="contact-btn">Submit</button>
     </form>
-
-
-
-
-    <!-- <div class="col-md-6 d-flex justify-content-center align-items-center">
-            <lottie-player src="https://assets5.lottiefiles.com/packages/lf20_gflsxfzt.json" background="transparent"
-                speed="1" class="contact-lottie" loop autoplay></lottie-player>
-        </div> -->
 </div>
